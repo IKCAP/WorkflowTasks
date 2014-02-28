c0dcd0bfcfa9ea9c38b37da921b92775baea81b4
Varun Ratnakar <varunratnakar@gmail.com>
2014-02-28 22:12:25 +0000

    New Files for ths 3 new category pages

---
+++ js/WTPerson.js
@@ -0,0 +1,51 @@
+var WTPerson = function(title, details, util, api ) {
+	this.title = title;
+	this.details = details;
+	this.util = util;
+	this.api = api;
+};
+
+WTPerson.prototype.display = function( item ) {
+	var me = this;
+	item.data('data', me.details);
+	var header = $j('<h2 style="margin-bottom:5px;margin-top:0px;padding-top:0px"></h2>').append('Recent Contributions');
+	item.append(header);
+
+	var contribs = me.details.WTPerson.contributions;
+	if(!contribs.length) {
+		item.append('No contributions by this person to the wiki.');
+		item.append('<br/><i>Note: If this person exists on the wiki, then add the "Has User ID" property to this page with its value set to the Wiki User ID.</i>'); 
+		return;
+	}
+
+	var topdiv = $j('<div class="scroller"></div>');
+	var table = $j('<table class="contribs"></table>');
+	var tr = $j('<tr></tr>');
+	tr.append('<th>Time</th>');
+	tr.append('<th>Page</th>');
+	tr.append('<th>Comment</th>');
+	table.append(tr);
+	
+	for(var i=0; i<contribs.length; i++) {
+		var contrib = contribs[i];
+
+		var tr = $j('<tr></tr>');
+		var timestr = contrib[2];
+		var year = timestr.substring(0,4);
+		var month = timestr.substring(4,6);
+		var day = timestr.substring(6,8);
+		var hour = timestr.substring(8,10);
+		var minute = timestr.substring(10,12);
+		var time = year+"-"+month+"-"+day+" "+hour+":"+minute;
+		tr.append('<td style="white-space:nowrap">'+time+'</td>');
+		tr.append('<td><a href="./'+contrib[0]+'">'+contrib[0]+'</a></td>');
+		var comment = contrib[1];
+		if(comment.length > 100) comment = comment.substring(0, 100) + '...';
+		if(comment == '') comment = '-- None --';
+		tr.append('<td>'+comment+'</td>');
+		table.append(tr);
+	}
+	topdiv.append(table);
+	item.append(topdiv);
+};
+
---
+++ js/WTUserProvidedData.js
@@ -0,0 +1,116 @@
+var WTUserProvidedData = function(title, details, util, api ) {
+	this.title = title;
+	this.details = details;
+	this.util = util;
+	this.api = api;
+};
+
+WTUserProvidedData.prototype.appendLinkItem = function( list, link, name ) {
+	if(!name) name = link;
+	var vlink = link.replace(/\s/g, '_');
+	list.append($j('<li></li>').append($j('<a href="'+vlink+'"></a>').append(name)));
+};
+
+WTUserProvidedData.prototype.getListItem = function( list, data ) {
+	var data_li = $j('<li></li>');
+
+	var me = this;
+	var delhref = $j('<a class="lodlink">[x]</a>');
+	// Minus [-] link's event handler
+	delhref.click( function(e) {
+		list.mask(lpMsg('Removing Data Name..'));
+		me.api.removeDataWikiLink( me.title, data.location, function(resp) {
+			list.unmask();
+			if(!resp || !resp.wtfacts) return;
+			if(resp.wtfacts.result == 'Success') {
+				me.add_data_link.css('display', '');
+				data_li.remove();
+			}
+		});
+	});
+	data_li.append(delhref).append(' ');
+	
+	var wflink = data.location.replace(/\s/g,'_');
+	data_li.append($j('<a href="./File:'+wflink+'"></a>').append("<b>"+wflink+"</b>"));
+
+	return data_li;
+};
+
+WTUserProvidedData.prototype.populateList = function( list, data ) {
+	var me = this;
+	if(data.location)
+		return list.append(this.getListItem(list, data));
+};
+
+WTUserProvidedData.prototype.getList = function( item, data ) {
+	var list = $j('<ul></ul>');
+	var me = this;
+
+	var ival = $j('<input style="width:30%" type="text" />');
+	var igo = $j('<a class="lodbutton">' + lpMsg('Go') + '</a>');
+	var icancel = $j('<a class="lodbutton">' + lpMsg('Cancel') + '</a>');
+
+	var add_data_li = $j('<li></li>').append($j('<div style="width:24px"></div>'));
+	add_data_li.append(ival).append(igo).append(icancel).hide();
+	list.append(add_data_li);
+
+	icancel.click(function( e ) {
+		ival.val('');
+		ival.data('val','');
+		add_data_li.hide();
+	});
+
+	igo.click(function( e ) {
+		var val = ival.data('val') ? ival.data('val') : ival.val();
+		add_data_li.hide();
+		if(!val) return; 
+		ival.val('');
+		ival.data('val','');
+
+		item.mask(lpMsg('Adding Data Link.. Please wait..'));
+		me.api.addDataWikiLink( me.title, val, function(response) {
+			item.unmask();
+			if(!response || !response.wtfacts || !response.wtfacts.newdetails) return; 
+			if(response.wtfacts.result == 'Success') {
+				var data = response.wtfacts.newdetails;
+				var data_li = me.getListItem(item, data.WTUserProvidedData);
+				me.add_data_link.css('display', 'none');
+				list.append(data_li);
+			}
+		});
+	});
+
+	if(data && data.Workflow) {
+		list.append(me.getListItem(item, data.Workflow));
+	}
+
+	item.data('list', list);
+	if(data && data.WTUserProvidedData) {
+		me.populateList(list, data.WTUserProvidedData);
+	}
+	item.data('list', list);
+	return list;
+};
+
+
+WTUserProvidedData.prototype.display = function( item ) {
+	var me = this;
+
+	item.data('data', me.details);
+
+	var list = me.getList( item, me.details );
+
+	me.add_data_link = $j('<a class="x-small lodbutton">' + lpMsg('Provide Data Name') + '</a>');
+	me.add_data_link.click(function( e ) {
+		list.find('li:first').css('display', '');
+	});
+
+	if(me.details && me.details.WTUserProvidedData.location)
+		me.add_data_link.css('display', 'none');
+
+	var header = $j('<h2 style="margin-bottom:5px;margin-top:0px;padding-top:0px"></h2>').append('User Provided Data');
+	var toolbar = $j('<div></div>').append(header).append(me.add_data_link);
+	item.append(toolbar);
+	item.append(list);
+};
+
